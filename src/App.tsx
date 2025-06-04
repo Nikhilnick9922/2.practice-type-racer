8a815ceb158e9710401ae098bbcf92882482f919
Nikhil Patil <nicks@Nikhils-iMac.local>
2025-06-04 18:03:56 +0530

    2. buttons - map is used to combine classes, filter used to make darker hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Button from "./Reusables/Button/Button";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="container">
+      <div className="btnGroup">
+        <Button color="#00bcf5" title="Refresh  XX" classes={["btnNormal"]} />
+        <i className="fa fa-refresh" aria-hidden="true"></i>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Reusables/Button/Button.tsx
@@ -0,0 +1,27 @@
+import styles from "./Button.module.css";
+
+interface Props {
+  title: string;
+  color?: string;
+  classes: string[];
+}
+
+const Button = ({ title, color, classes }: Props) => {
+  const classNames = [
+    styles.btn, // Always include the base button style
+    ...(classes || []).map((cls) => styles[cls]), // Map each class string to its CSS module counterpart
+  ].join(" ");
+  return (
+    <button style={{ backgroundColor: `${color}` }} className={classNames}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
+
+//  background-color: #67ac5b; green
+//  background-color: #219ab0;   blue
+//  background-color: #ffd61e; yellow
+//  background-color : #e68200; orange
+//  background-color : #ffb11b; light orange
