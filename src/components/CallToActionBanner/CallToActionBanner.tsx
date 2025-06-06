77f46a85ba784517ac1afe549ebece9278856e71
Nikhil Patil <nicks@Nikhils-iMac.local>
2025-06-06 12:29:35 +0530

    5. CallToActionBanner created - great use of variable at global level css

--- src/components/CallToActionBanner/CallToActionBanner.tsx
+++ src/components/CallToActionBanner/CallToActionBanner.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Button from "../../Reusables/Button/Button";
+import "./callToACtionBanner.css";
 
 const CallToActionBanner = () => {
-  return <div>CallToActionBanner</div>;
+  return (
+    <div className="callContainer">
+      <div className="headingText">
+        <h2 className="callHeading">
+          Record your races with a TypeRacer Account!
+        </h2>
+        <span className="callText">
+          Save your race history and scores. Customize your profile and car.
+          It's free, why not?
+        </span>
+      </div>
+      <div className="btnContainer">
+        <Button
+          title="Create Your Account"
+          color="#e68200"
+          classes={["btnLarge"]}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CallToActionBanner;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "font-awesome/css/font-awesome.css";
+import "./styles/variables.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
