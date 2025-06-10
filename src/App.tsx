4b3c08562282d745102778638cc2260f7b09d9bb
Nikhil Patil <nicks@Nikhils-iMac.local>
2025-06-10 15:22:21 +0530

    11. put all together

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import "./App.css";
+import CallToActionBanner from "./components/CallToActionBanner/CallToActionBanner";
 import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import HighScoreDisplay from "./components/HighScroreDisplay/HightScoreDisplay";
+import PracticeMode from "./components/PracticeMode/PracticeMode";
+import RandomText from "./components/RandomText/RandomText";
 
 const App = () => {
   return (
     <div className="container">
+      <Header />
+      <CallToActionBanner />
+      <RandomText />
+      <PracticeMode />
+      <HighScoreDisplay />
       <Footer />
     </div>
   );
